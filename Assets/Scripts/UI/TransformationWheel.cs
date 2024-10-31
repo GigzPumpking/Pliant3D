f15b771cfaa6bba83f9636036166a0e44b8680b6
Dominic London <londonrdominic@gmail.com>
2024-10-31 10:23:40 -0700

    Start of new transform wheel
    UI elements transformation wheel made and starting script made

---
+++ Assets/Scripts/UI/TransformationWheel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformationWheel : MonoBehaviour
+{
+    public Color hoverHighlight;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Select()
+    {
+        
+    }
+}

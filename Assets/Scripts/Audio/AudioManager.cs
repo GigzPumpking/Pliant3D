a893548c4b819af007180fccb21f3b35386bfb8d
Gigz <gigzpumpking@gmail.com>
2025-12-13 10:50:31 -0800

    Removed SceneManagement import

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
 
 [System.Serializable]
 public class AudioData

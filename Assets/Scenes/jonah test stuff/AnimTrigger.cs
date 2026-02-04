5d2f69cc8c8274836e329f51ca54c94b6fb2d2b3
Jonah Vera Cruz <hawaiian9009@users.noreply.github.com>
2026-02-04 13:39:42 -1000

    furnished level 1-1 + animation script
    what has been done:
    
    - added props to level one
    - fixed and modified an old animation script with the help of google.

--- Assets/Scenes/jonah test stuff/AnimTrigger.cs
+++ Assets/Scenes/jonah test stuff/AnimTrigger.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
 
-public class AnimTrigger : MonoBehaviour
+public class AnimTrigger : MonoBehaviour 
 {
     [SerializeField] private Animator myAnimationController;
+    [SerializeField] private string targetTag = "Player";
+    [SerializeField] private string parameterName = "test";
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) 
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(targetTag)) 
         {
-            myAnimationController.SetBool("test", true);
+            myAnimationController.SetBool(parameterName, true);
         }
     }
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other) 
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(targetTag)) 
         {
-            myAnimationController.SetBool("test", false);
-         }
-     } 
+            myAnimationController.SetBool(parameterName, false);
+        }
+    }
+
+
+
+    
 }

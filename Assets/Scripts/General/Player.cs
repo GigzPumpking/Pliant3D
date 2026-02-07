fd0885db92e624bca55757b4a5d796872efa3754
Ryan R <33332787+RyannR9375@users.noreply.github.com>
2026-02-07 00:05:04 -0800

    Update Player.cs

--- Assets/Scripts/General/Player.cs
+++ Assets/Scripts/General/Player.cs
@@ -254,11 +254,12 @@ public class Player : KeyActionReceiver<Player>
         { 
             isMoving = false;
         }
-        
+
         if (transform.position.y < outOfBoundsY && !GameManager.Instance.isGameOver
-                                                && SceneManager.GetActiveScene().name != "2-0 Meri" 
+                                                && SceneManager.GetActiveScene().name != "2-0 Meri"
                                                 && SceneManager.GetActiveScene().name != "3-0 Carrie"
-                                                && SceneManager.GetActiveScene().name != "11-0 Thanks")
+                                                && SceneManager.GetActiveScene().name != "11-0 Thanks"
+                                                && SceneManager.GetActiveScene().name != "0 Main Menu") ;
         {
             Debug.LogWarning("s::" + SceneManager.GetActiveScene().name);
             Debug.LogWarning("Game Over from Player.cs");

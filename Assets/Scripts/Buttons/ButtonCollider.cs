08dc0eb4da26ba7eb73f4cab5b4bfa7cfb46c744
GigzPumpking <gigzpumpking@gmail.com>
2026-02-19 19:39:18 -0800

    button fix

--- Assets/Scripts/Buttons/ButtonCollider.cs
+++ Assets/Scripts/Buttons/ButtonCollider.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class ButtonCollider : MonoBehaviour
 {
+    [Tooltip("The ButtonScript to activate. Auto-detected on this object or parent if left empty.")]
+    [SerializeField] private ButtonScript buttonScript;
+
+    private void Start()
+    {
+        if (buttonScript == null)
+            buttonScript = GetComponentInParent<ButtonScript>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag != "Player") return;
         else if (Player.Instance.GetTransformation() != Transformation.FROG) return;
 
-        EventDispatcher.Raise<PressButton>(new PressButton());
+        if (buttonScript != null) buttonScript.Press();
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag != "Player") return;
         else if (Player.Instance.GetTransformation() != Transformation.FROG) return;
-        
-        EventDispatcher.Raise<ReleaseButton>(new ReleaseButton());
+
+        if (buttonScript != null) buttonScript.Release();
     }
 }

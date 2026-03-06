f4119affe9990367f698a7999d519a281bee0711
Ryan R <33332787+RyannR9375@users.noreply.github.com>
2026-03-06 00:31:02 -0800

    audio sliders should work?

--- Assets/Scripts/UI/ControlsMenu.cs
+++ Assets/Scripts/UI/ControlsMenu.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -7,6 +6,8 @@ using UnityEngine.InputSystem;
 
 public class ControlsMenu : SwappableMenu
 {
+    [SerializeField] private TextMeshProUGUI controlsText;
+    
     private enum ControlType
     {
         Keyboard,
@@ -19,5 +20,39 @@ public class ControlsMenu : SwappableMenu
     {
         base.OnEnable();
         ((SwappableMenu)this).RegisterToUIManager(); //Register to the UIManager as a swappable menu
+    }
+
+    private void Start()
+    {
+        if (InputManager.Instance?.ActiveDeviceType == "Keyboard" || InputManager.Instance?.ActiveDeviceType == "Mouse")
+        {
+            DisplayController();
+        }
+        else
+        {
+            DisplayKeyboard();
+        }
+    }
+
+    public void SwapControls()
+    {
+        if (InputManager.Instance?.ActiveDeviceType == "Keyboard" || InputManager.Instance?.ActiveDeviceType == "Mouse") 
+        {
+            DisplayController();
+        }
+        else
+        {
+            DisplayKeyboard();
+        }
+    }
+
+    private void DisplayController()
+    {
+        controlsText.text = "GAMEPAD";
+    }
+
+    private void DisplayKeyboard()
+    {
+        controlsText.text = "KEYBOARD";
     }
 }

c78eb5a252c73c26dec7647e07703a97332228a8
Zoe <zandrews001@gmail.com>
2026-05-11 20:51:13 -0700

    Pause menu update
    added main menu button
    switched around button for pause menu layout

--- Assets/Scripts/Scene/NewSceneChecker.cs
+++ Assets/Scripts/Scene/NewSceneChecker.cs
@@ -86,6 +86,11 @@ public class NewSceneChecker : MonoBehaviour
         UIManager.Instance?.FadeOut();
         SceneManager.LoadScene("0 Main Menu");
     }
+    
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("0 Main Menu");
+    }
 
     public void QuitGame()
     {
